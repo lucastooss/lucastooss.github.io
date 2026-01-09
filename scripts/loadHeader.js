4f6a76e924819d7aed0e5550c3603d2871cda5e4
lucastooss <lucastooss@gmail.com>
2026-01-09 21:35:24 +0100

    added feature

--- scripts/loadHeader.js
+++ scripts/loadHeader.js
@@ -7,14 +7,23 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(htmlContent => {
             headerPlaceholder.innerHTML = htmlContent;
 
+            const header = headerPlaceholder.querySelector('.main-header');
             const menuBtn = document.getElementById('mobile-menu-btn');
             const navMenu = document.getElementById('nav-menu');
             const body = document.body;
 
+            // Scroll-Funktion: Klasse hinzufügen/entfernen
+            window.addEventListener('scroll', () => {
+                if (window.scrollY > 50) {
+                    header.classList.add('scrolled');
+                } else {
+                    header.classList.remove('scrolled');
+                }
+            });
+
             if (menuBtn && navMenu) {
                 menuBtn.addEventListener('click', function() {
                     navMenu.classList.toggle('is-active');
-                    
                     body.classList.toggle('menu-open');
                 });
 
