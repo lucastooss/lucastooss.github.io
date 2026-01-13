73cc65542b83ef22062f0adf13fb5e5391e684d5
lucastooss <lucastooss@gmail.com>
2026-01-13 18:41:01 +0100

    reset

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-require('dotenv').config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); 
 const express = require('express');
-const axios = require('axios')(process.env.FORMSPREE_URL);
 const cors = require('cors');
 const app = express();
 
-// Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
 
 app.post('/create-checkout-session', async (req, res) => {
     try {
@@ -32,32 +27,5 @@ app.post('/create-checkout-session', async (req, res) => {
 });
 
 
-app.post('/send-contact', async (req, res) => {
-    try {
-        const formspreeUrl = process.env.FORMSPREE_URL;
 
-        if (!formspreeUrl) {
-            console.error('Fehler: FORMSPREE_URL Umgebungsvariable ist nicht gesetzt.');
-            return res.status(500).send('Server-Konfigurationsfehler.');
-        }
-
-        const response = await axios.post(formspreeUrl, req.body, {
-            headers: {
-                'Accept': 'application/json'
-            }
-        });
-
-        if (response.status === 200) {
-            res.redirect('https://lucastooss.github.io/confirmed/index.html');
-        } else {
-            res.status(500).send('Fehler beim Versand über Formspree.');
-        }
-    } catch (error) {
-        console.error('Fehler bei der Formspree-Weiterleitung:', error.message);
-        res.status(500).send('Interner Serverfehler.');
-    }
-});
-
-// Server Start
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
+app.listen(process.env.PORT || 3000, () => console.log('Server läuft'));
